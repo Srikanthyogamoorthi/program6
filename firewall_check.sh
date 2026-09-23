8c0600fd3535ffc8aac475bd08f02074b6da5021
srikanth <srikanthy476@gmail.com>
2026-09-23 12:24:06 +0530

    Refactor firewall_check.sh to remove sudo and add TODOs
    Updated firewall_check.sh to remove 'sudo' and added TODO comments for completion.

--- firewall_check.sh
+++ firewall_check.sh
@@ -5,19 +5,24 @@
 # Register Number:
 
 # 1. Check firewalld service status
-sudo systemctl status firewalld
+systemctl status firewalld
 
 # 2. Start firewalld
-sudo systemctl start firewalld
+# TODO: Complete the command
+systemctl start firewalld
 
 # 3. Enable firewalld at boot
-sudo systemctl enable firewalld
+# TODO: Complete the command
+systemctl enable firewalld
 
 # 4. Check firewall state
-sudo firewall-cmd --state
+# TODO: Complete the command
+firewall-cmd --state
 
 # 5. Get default firewall zone
-sudo firewall-cmd --get-default-zone
+# TODO: Complete the command
+firewall-cmd --get-default-zone
 
 # 6. Get active firewall zones
-sudo firewall-cmd --get-active-zones
+# TODO: Complete the command
+firewall-cmd --get-active-zones
